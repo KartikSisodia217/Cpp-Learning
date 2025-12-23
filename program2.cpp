8a6261a87db37a460c504144906a743637ff00f1
KartikSisodia217 <sisodiakartik21@gmail.com>
2025-12-23 16:16:31 +0530

    my commit on 23/12/2025

---
+++ program2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+  int a=3, b=674, c=1993;
+
+  cout<<"The value of a without setw is: "<<a<<endl;
+  cout<<"The value of b without setw is: "<<b<<endl;
+  cout<<"The value of c without setw is: "<<c<<endl;
+  
+  cout<<"The value of a with setw is: "<<setw(4)<<a<<endl; //ensures that the text ahead occupies 4 bits
+  cout<<"The value of b with setw is: "<<setw(4)<<b<<endl;
+  cout<<"The value of c with setw is: "<<setw(4)<<c<<endl;
+  return 0;
+}
