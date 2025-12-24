c18a638ebae827297864c9b0ba383873dc8f7720
KartikSisodia217 <sisodiakartik21@gmail.com>
2025-12-25 00:40:07 +0530

    my commit on 25/12/2025

---
+++ loops.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  cout<<"FOR LOOP :"<<endl;
+  for (int i = 1;i<=10;i++ ){
+    cout<<"i="<<i<<endl;
+  }
+cout<<endl<<endl<<endl;
+cout<<"WHILE LOOP :"<<endl;
+  int j = 1;
+  while(j<=15){
+    cout<<"j="<<j<<endl;
+    j++;
+  }
+
+
+  return 0;
+}
