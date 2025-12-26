d1cc00319bbb75ba9ddac4b321821a71aa8f3ce5
KartikSisodia217 <sisodiakartik21@gmail.com>
2025-12-26 23:37:11 +0530

    my commit on 26/12/2025

--- struct.cpp
+++ struct.cpp
@@ -8,7 +8,14 @@ struct student
   int marks;
   char grade;
 };
-
+  
+union money // A union lets you store different data types in the same memory location, but only one of them can be used at a time.
+{
+  int rice; // int: 4 byte, char: 1 byte, float: 4 byte. if we used struct, we'd require 9 byte of data but with union we only need 4 byte of data.
+  char car;
+  float pounds;
+};
+  
 int main(){
   struct student Kartik;
   Kartik.rollNo = 14;
@@ -18,6 +25,19 @@ int main(){
   cout<<"Kartik's roll no = "<<Kartik.rollNo<<endl;
   cout<<"Kartik's marks = "<<Kartik.marks<<endl;
   cout<<"Kartik's grade = "<<Kartik.grade<<endl;
+  cout<<endl<<endl;
+
+  union money m1;
+  m1.rice = 34;
+  m1.car = 'c';
+  cout<<"cost of rice: " <<m1.rice<<endl;
+  cout<<"car: "<<m1.car<<endl;
+
+
+  enum meal{ breakfast, lunch, dinner};
+  cout<<breakfast<<endl; 
+  cout<<lunch<<endl;
+  cout<<dinner<<endl;
   
   return 0;
 }
