e0693f3aa7c5a187800567b2570c8813751c80a7
KartikSisodia217 <sisodiakartik21@gmail.com>
2025-12-30 21:33:03 +0530

    my commit on 30/12/2025

---
+++ inline_functions.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+inline int product (int x,int y){
+  static int c = 0; // this executes only once
+  c = c+1; // Next time the function will run, it will remember this value. 
+  return x*y*c;
+}
+
+
+int main(){
+  int a,b;
+  cout<<"Enter the value of a and b"<<endl;
+  cout<<"a: ";
+  cin>>a;
+  cout<<"b: ";
+  cin>>b;
+  cout<<endl;
+ 
+  cout<<"The product of a and b is: "<<product(a,b)<<endl; // the same function is executed multiple times., you can convert it to inline function as it directly shows the output instead of copying the variables and doing all the steps on them everytime
+  cout<<"The product of a and b is: "<<product(a,b)<<endl; // try to make small functions (1-2 lines) inline functions
+  cout<<"The product of a and b is: "<<product(a,b)<<endl;
+  cout<<"The product of a and b is: "<<product(a,b)<<endl;
+  cout<<"The product of a and b is: "<<product(a,b)<<endl;
+  cout<<"The product of a and b is: "<<product(a,b)<<endl;
+  cout<<"The product of a and b is: "<<product(a,b)<<endl;
+  cout<<"The product of a and b is: "<<product(a,b)<<endl;
+  cout<<"The product of a and b is: "<<product(a,b)<<endl;
+  cout<<"The product of a and b is: "<<product(a,b)<<endl;
+  cout<<"The product of a and b is: "<<product(a,b)<<endl;
+  return 0;
+}
