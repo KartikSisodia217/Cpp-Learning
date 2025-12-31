90c9ce1e8caf6d04011756d43508134a1f9c78ff
KartikSisodia217 <sisodiakartik21@gmail.com>
2025-12-31 18:11:44 +0530

    my commit on 31/12/2025

--- recursion.cpp
+++ recursion.cpp
@@ -5,16 +5,26 @@ int factorial(int n){
   if(n<=1){
     return 1;
   }
-  else{
-    return n * factorial(n-1);
+  
+  return n * factorial(n-1);
+  
+};
+
+int fib(int n){
+  if(n<3){
+    return n-1;
   }
+  return fib(n-2)+fib(n-1);
 };
 
 int main(){
   //Factorial of a number
-  int a;
+  int a,b;
   cout<<"Enter the value of a: ";
   cin>>a;
-  cout<<"The factorial of a is: "<<factorial(a);
+  cout<<"The factorial of "<<a<<" is: "<<factorial(a)<<endl;
+  cout<<"Enter the value of b: ";
+  cin>>b;
+  cout<<"The fibonacci number at position "<<b<<" is: "<<fib(b)<<endl;
   return 0;
 }
